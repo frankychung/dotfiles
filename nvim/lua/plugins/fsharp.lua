64e19a7e18daa49ec8363190cb7e9ad2eea12c4b
Franky Chung <franchun@gmail.com>
2025-10-30 17:49:56 +0900

    fsharpie

--- nvim/lua/plugins/fsharp.lua
+++ nvim/lua/plugins/fsharp.lua
@@ -1,54 +1,10 @@
 return {
-  -- Ionide-vim for F# support
-  {
-    "ionide/Ionide-vim",
-    ft = { "fsharp" },
-    config = function()
-      -- Configure F# backend to use Neovim's built-in LSP
-      vim.g["fsharp#backend"] = "nvim"
-      vim.g["fsharp#automatic_workspace_init"] = 1
-      vim.g["fsharp#workspace_mode_peek_deep_level"] = 2
-      vim.g["fsharp#exclude_project_directories"] = { "paket-files" }
-    end,
-  },
-
-  -- LSP configuration for F#
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         fsautocomplete = {
           cmd = { "fsautocomplete", "--background-service-enabled" },
-          filetypes = { "fsharp" },
-          root_dir = function(fname)
-            return require("lspconfig.util").root_pattern("*.sln", "*.fsproj", ".git")(fname)
-          end,
-          init_options = {
-            AutomaticWorkspaceInit = true,
-          },
-          settings = {
-            FSharp = {
-              keywordsAutocomplete = true,
-              externalAutocomplete = false,
-              linter = true,
-              unionCaseStubGeneration = true,
-              unionCaseStubGenerationBody = "failwith \"Not Implemented\"",
-              recordStubGeneration = true,
-              recordStubGenerationBody = "failwith \"Not Implemented\"",
-              interfaceStubGeneration = true,
-              interfaceStubGenerationObjectIdentifier = "this",
-              interfaceStubGenerationMethodBody = "failwith \"Not Implemented\"",
-              unusedOpensAnalyzer = true,
-              unusedDeclarationsAnalyzer = true,
-              simplifyNameAnalyzer = true,
-              resolveNamespaces = true,
-              enableReferenceCodeLens = true,
-              enableAnalyzers = true,
-              analyticsAutocomplete = {
-                enabled = false,
-              },
-            },
-          },
         },
       },
     },
@@ -63,5 +19,17 @@ return {
         vim.list_extend(opts.ensure_installed, { "fsharp" })
       end
     end,
+  },
+
+  -- Neotest adapter for .NET
+  { "nsidorenco/neotest-vstest" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      log_level = vim.log.levels.DEBUG,
+      adapters = {
+        ["neotest-vstest"] = {},
+      },
+    },
   },
 }
--- zshrc
+++ zshrc
@@ -202,3 +202,9 @@ fi
 if command -v zoxide &>/dev/null; then
     alias cd="z"
 fi
+
+PATH="/Users/franky/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/Users/franky/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/Users/franky/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/Users/franky/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/Users/franky/perl5"; export PERL_MM_OPT;
